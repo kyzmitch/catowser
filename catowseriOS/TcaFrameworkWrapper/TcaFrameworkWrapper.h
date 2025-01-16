42f84a2ad8cc7e11f0c31fc8e82c58aa60d8d851
Andrei Ermoshin <iermoshin@gmail.com>
2025-01-16 23:36:52 +0300

    Fix linking issue

---
+++ catowseriOS/TcaFrameworkWrapper/TcaFrameworkWrapper.h
@@ -0,0 +1,19 @@
+//
+//  TcaFrameworkWrapper.h
+//  TcaFrameworkWrapper
+//
+//  Created by Andrey Ermoshin on 16.01.2025.
+//  Copyright © 2025 Cotton (Catowser). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TcaFrameworkWrapper.
+FOUNDATION_EXPORT double TcaFrameworkWrapperVersionNumber;
+
+//! Project version string for TcaFrameworkWrapper.
+FOUNDATION_EXPORT const unsigned char TcaFrameworkWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TcaFrameworkWrapper/PublicHeader.h>
+
+
